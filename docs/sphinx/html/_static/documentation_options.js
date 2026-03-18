caefe8f1bee81d9652cb36941b374cb2a54d8364
Steven de Oliveira <steven.de-oliveira@ocamlpro.com>
2026-03-18 16:17:04 +0100

    Documentation

---
+++ docs/sphinx/html/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: 'doc (36e9a753) (2026/03/18 15:55)',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
